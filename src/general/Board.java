ecbaf40d62c7434d835cf18d681e1c415536d301
Juan Li Puma Finnemore <jlipumafinnemore@lab78.it.itba.edu.ar>
2015-10-14 17:13:43 -0300

    First lines of code

--- src/general/Board.java
+++ src/general/Board.java
@@ -1,9 +1,42 @@
 package general;
 
 public class Board {
+	public static final int[] POINTS = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}; 
+	public static enum Direction {DOWN, RIGHT};
+	public static final int SIZE = 15;
+	private char[][] spaces;
 
 	public Board() {
-		// TODO Auto-generated constructor stub
+		spaces = new char[SIZE][SIZE];
+		clearBoard();
 	}
-
+	
+	public boolean isOccupied(int x, int y) {
+		return spaces[y][x] != ' ';
+	}
+	
+	public int getScore() {
+		int result = 0;
+		for(char[] row : spaces) {
+			for(char c : row) {
+				if(c != ' ') {
+					result += POINTS[c-'A'];
+				}
+			}
+		}
+		return result;
+	}
+	
+	public boolean isValidMove() {
+		//TODO
+		return false;
+	}
+	
+	private void clearBoard() {
+		for(int row = 0; row < SIZE; row++) {
+			for(int col = 0; col < SIZE; col++) {
+				spaces[row][col] = ' ';
+			}
+		}
+	}
 }
