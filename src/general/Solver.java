470aa1a0912488d9e7519237310a67960b71188f
julianrod94 <julrodriguez@itba.edu.ar>
2015-10-23 16:22:04 -0300

    helperupdate

--- src/general/Solver.java
+++ src/general/Solver.java
@@ -1,6 +1,10 @@
 package general;
 
+import general.BoardState.Direction;
+
 import java.util.Set;
+
+import solving.Helper;
 
 public class Solver {
 	private Set<String> dictionary;
@@ -8,8 +12,8 @@ public class Solver {
 	private char[][] bestAnswer;
 	
 	
-	public Solver(int[] letters){
-		this.letters=letters;
+	public Solver(Set<String> dictionary){
+		this.dictionary=dictionary;
 	}
 
 
@@ -25,5 +29,43 @@ public class Solver {
 
 	public char[][] getBestAnswer() {
 		return bestAnswer;
+	}
+	public BoardState backTracking(Set<String> dictionary, int[] letters){
+		BoardState board = new BoardState(letters);
+		this.dictionary = dictionary;
+		PossibleWordsIterator it = new PossibleWordsIterator(dictionary.iterator(), letters);
+		BoardState result;
+		BoardState bestBoard = board;
+		while(it.hasNext()){
+			String word = it.next();
+			for(int i=0 ; i<word.length(); i++){
+				Move movement = new Move(word, 7 + i, 7, Direction.RIGHT);
+				board.doMove(movement);
+				result = backTracking(board, bestBoard);
+				if(result.compareTo(bestBoard)>0){
+					bestBoard = result;
+				}
+				board.undoMove(movement);
+			}
+		}
+		return bestBoard;
+	}
+	
+	private BoardState backTracking(BoardState currentBoard, BoardState bestBoard){
+		Set<Move> movements=Helper.getPossibleMoves(currentBoard, this.dictionary);
+		if(movements.isEmpty()){
+			if(currentBoard.compareTo(bestBoard)>0){
+				return currentBoard;
+			}
+			return bestBoard;
+		}
+		for(Move movement: movements){
+			currentBoard.doMove(movement);
+			BoardState result = backTracking(currentBoard, bestBoard);
+			if(result.compareTo(bestBoard)>0){
+				bestBoard = result;
+			}
+		}
+		return bestBoard;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import general.BoardState;
-import general.BoardState.Direction;
-import general.Move;
+import general.Solver;
 import general.Validator;
 import gui.StateVisualizer;
-import solving.Helper;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
@@ -22,21 +20,11 @@ public class Main {
 		dictionary.add("MONITOR");
 		dictionary.add("QUESO");
 		Validator.setDictionary(dictionary);
-		StateVisualizer v = new StateVisualizer();
 		
-		BoardState b = new BoardState(new int[] {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3});
-		Move m1 = new Move("CASA", 5, 0, Direction.DOWN);
-		System.out.println("Moving: " + m1);
-		b.doMove(m1);
-		v.print(b.toPrettyString());
-		Set<Move> moves = Helper.getPossibleMoves(b, dictionary);
-		Move m2 = new Move("TABLA", 4, 3, Direction.RIGHT);
-		assert moves.contains(m2);
-		b.doMove(m2);
+		int[] letters = new int[] {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+		Solver solver = new Solver(dictionary);
+		System.out.println(solver.backTracking(dictionary,letters));
 		
-		for(int i = 0; i < 100; i++) {
-			v.print(b.toPrettyString());
-		}
 	}
 
 }
