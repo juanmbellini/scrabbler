253da5c6ef55fad2c8cfa17a9bdb2298a5bc872f
julianrod94 <julrodriguez@itba.edu.ar>
2015-10-27 00:16:36 -0300

    EnTeoriaTodoListo4.0

--- src/general/Validator.java
+++ src/general/Validator.java
@@ -29,7 +29,8 @@ public class Validator {
 		if(!isWithinBounds(b, m) || !boardHasEnoughLettersFor(b, m)) {
 			return false;
 		}
-		boolean matches = false;;
+		int overlappingSpaces = 0;
+		boolean matches = false;
 		if(m.getDirection() == Direction.DOWN) {
 			for(int i=y; i < y + word.length(); i++){   		//se fija si 
 				if(b.isOccupied(x-1, i) || b.isOccupied(x+1, i)){ //no forma palabras nuevas y si lo 
@@ -49,7 +50,11 @@ public class Validator {
 				}
 				if(b.isOccupied(x, i)){
 					matches = true;
+					overlappingSpaces++;
 				}
+			}
+			if(overlappingSpaces == word.length()){
+				return false;
 			}
 			if((y > 0 && spaces[y-1][x]!=' ') || spaces[y+m.getWord().length()][x]!=' '){
 				StringBuffer wordaux = new StringBuffer();
